87a0fd67d17849a6abec11cd8db4ff32a0f00f2c
lim lee jing <limleejing@lims-MacBook-Air.local>
2022-09-11 18:34:43 +0800

    Install and configure tailwind, added  css ,html

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,ts}'], //configure the paths to all of your HTML templates, typescript components, and any other source files that contain Tailwind class names.
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
