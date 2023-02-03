152d908c0f91dd81be9adafd921e0459f831aef5
Evan1115 <jing111597@gmail.com>
2023-02-04 05:14:18 +0800

    get user id from authemtication

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,6 +1,6 @@
 export default interface IUser {
     name: string,
-    password: string,
+    password?: string,
     email: string,
     age: number,
     phoneNumber: string
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,26 +1,41 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+} from '@angular/fire/compat/firestore';
 import IUser from '../models/user.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private auth: AngularFireAuth, private db: AngularFirestore) {}
+  private userCollection: AngularFirestoreCollection<IUser>;
+
+  constructor(private auth: AngularFireAuth, private db: AngularFirestore) {
+    this.userCollection = this.db.collection('users'); //create  a reference to firestore collection
+  }
 
   public async createUser(userData: IUser) {
     const { name, email, password, age, phoneNumber } = userData;
+
+    //firebase will automatically include the token once we created a account (authentication)
     const userCreated = await this.auth.createUserWithEmailAndPassword(
       email as string,
       password as string
     );
-    this.db.collection('users').add({
+
+    //so everytime u send a request, it will contain token
+    await this.userCollection.doc(userCreated.user?.uid).set({
       name: name,
       email: email,
       age: age,
       phoneNumber: phoneNumber,
     });
+
+    await userCreated.user?.updateProfile({
+      displayName: name
+    })
     console.log(userCreated);
   }
 }
